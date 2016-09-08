d6f73d6b076aaf88884a2acd2cfc8fa2643564d3
tgrave <sts@auto-jem.com>
2016-09-08 15:34:35 +0300

    Now it's rubocop tested.

--- lib/yandex_image_moderation.rb
+++ lib/yandex_image_moderation.rb
@@ -1,12 +1,13 @@
-require "yandex_image_moderation/version"
+require 'yandex_image_moderation/version'
 
+# YandexImageModeration gem code
 module YandexImageModeration
   def moderate(file)
     return if file.nil?
 
+    _name, _path = *get_file_params(file)
   end
 
   def get_file_params(file)
-
   end
 end
